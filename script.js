5204370d81a702054ae7e4a8fc7bdd8b8fca2ea8
Sandough <jose.sndov@gmail.com>
2024-08-18 04:05:07 -0500

    Play is dynamic. Changes from play to pause and back, and is functional

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 // Remember to add all the constants up here!
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nestBtn = document.getElementById('prev');
+
 // Music
 
 // Check if Playing
+let isPlaying = false;
 
 // Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause')
+  music.play();
+}
 
 // Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play')
+  music.pause();
+}
 
 // Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
 
 // Current Song
 
